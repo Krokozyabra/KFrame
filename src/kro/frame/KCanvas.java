e2fc07b142378cbceff89e9e55c1c91041271688
Krokozyabra <arkachist@gmail.com>
2015-11-24 15:56:18 +0300

    Update KCanvas.java

--- src/kro/frame/KCanvas.java
+++ src/kro/frame/KCanvas.java
@@ -1,9 +1,25 @@
 package kro.frame;
 
 import java.awt.Canvas;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class KCanvas extends Canvas {
 	public KCanvas(){
 		setFocusable(true);
 	}
+	
+	public void addMouseListener(MouseListener mouseListener){
+		addMouseListener(mouseListener);
+	}
+	
+	public void addKeyListener(KeyListener keyListener){
+		addKeyListener(keyListener);
+	}
+	
+	public void addMouseMotionListener(MouseMotionListener mouseMotionListener){
+		addMouseMotionListener(mouseMotionListener);
+	}
+	
 }
